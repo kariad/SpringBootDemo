2c8fe0351d20cea841c0e0d98f85e49906ec7128
Daiki Katayama <karuiad.venezia@gmail.com>
2018-12-06 15:23:05 +0900

    initial commit.

---
+++ src/main/kotlin/com/example/demo/GreetingController.kt
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+    val coutner = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(coutner.incrementAndGet(), "Hello, $name")
+
+}
+
+data class Greeting(val id: Long, val content: String)
